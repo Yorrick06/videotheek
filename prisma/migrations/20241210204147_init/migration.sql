24560051378ed09f5145ebff1f3e1f206d5611b0
Yorrick06 <yorrick06@casema.nl>
2024-12-10 21:46:49 +0100

    Mysql to SQLite

---
+++ prisma/migrations/20241210204147_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "video" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Title" TEXT NOT NULL,
+    "Genre" TEXT NOT NULL,
+    "Release Year" INTEGER NOT NULL,
+    "Description" TEXT NOT NULL
+);
